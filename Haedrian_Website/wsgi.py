28a83e51426ca291134c43ed464c6d8ce053721f
James Rowe <jroweboy@gmail.com>
2015-03-12 00:34:12 -0600

    Typed the wrong path for the settings file

--- Haedrian_Website/wsgi.py
+++ Haedrian_Website/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 # NOTE: you should be using manage.py to run locally
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "haedrian.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Haedrian_Website.settings.production")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()

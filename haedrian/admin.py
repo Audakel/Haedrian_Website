f80d016972ee60d82ef9378541ee558508a3d263
James Rowe <jroweboy@gmail.com>
2015-05-18 14:22:37 -0600

    Changes to the backend
    Added a create user endpoint. Fiddled with django organizations and django
    mptt but it looks like we are going to go a different route isntead.

--- haedrian/admin.py
+++ haedrian/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from haedrian.models import BetaApplicant, UserData
+# Account, AccountUser
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('name','country','reason')
@@ -9,3 +10,22 @@ class UserDataAdmin(admin.ModelAdmin):
 
 admin.site.register(BetaApplicant, PersonAdmin)
 admin.site.register(UserData, UserDataAdmin)
+
+# from django.contrib import admin
+# from mptt.admin import MPTTModelAdmin
+# from haedrian.models import Node
+
+# class CustomMPTTModelAdmin(MPTTModelAdmin):
+#     verbose_name = 'Activity'
+
+# admin.site.register(Node, CustomMPTTModelAdmin)
+
+# class AccountUserAdmin(admin.ModelAdmin):
+#     # list_display = ('name',)
+#     pass
+
+# admin.site.unregsiter(Organization)
+# admin.site.unregsiter(OrganizationUser)
+# admin.site.unregsiter(OrganizationOwner)
+# admin.site.register(Account)
+# admin.site.register(AccountUser, AccountUserAdmin)
--- haedrian/urls.py
+++ haedrian/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url, include
 from haedrian import views
 
 urlpatterns = patterns('',
-    url(r'^register', views.create_account, name='register'),
-    url(r'^', views.index, name='index'),
+    url(r'^register/$', views.create_account, name='register'),
+    url(r'^$', views.index, name='index'),
 )

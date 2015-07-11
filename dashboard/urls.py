4f247b40ce6f96dc33011d7d97834abf95a536ad
Logan Bentley <loganbentley22@gmail.com>
2015-07-10 18:11:07 -0600

    started adding stuff for admin dash

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,6 @@ from dashboard import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^admin', views.admin_dashboard, name='admin'),
     url(r'^(?P<page>.+)', views.index),
 )
--- dashboard/views.py
+++ dashboard/views.py
@@ -5,3 +5,22 @@ def index(request, page=''):
     if page == '':
         page = 'index.html'
     return render(request, 'dashboard/%s' %page)
+
+def admin_dashboard(request):
+    xdata = ["Apple", "Apricot", "Avocado", "Banana", "Boysenberries", "Blueberries", "Dates", "Grapefruit", "Kiwi", "Lemon"]
+    ydata = [52, 48, 160, 94, 75, 71, 490, 82, 46, 17]
+    chartdata = {'x': xdata, 'y': ydata}
+    charttype = "pieChart"
+    chartcontainer = 'piechart_container'
+    data = {
+        'charttype': charttype,
+        'chartdata': chartdata,
+        'chartcontainer': chartcontainer,
+        'extra': {
+            'x_is_date': False,
+            'x_axis_format': '',
+            'tag_script_js': True,
+            'jquery_on_ready': False,
+        }
+    }
+    return render(request, 'piechart.html', data)

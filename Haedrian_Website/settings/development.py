d0181c4e92533700e46a9ef4fd926ee230ec967d
Aquila Haedrian <aquila@haedrian.io>
2015-07-21 14:09:26 -0600

    Dev settings

--- Haedrian_Website/settings/development.py
+++ Haedrian_Website/settings/development.py
@@ -8,7 +8,9 @@ MANAGERS = ADMINS
 
 SECRET_KEY = 'TEST'
 
-COINS_BASE_URL = 'https://sandbox.coins.ph'
+COINS_BASE_URL = 'https://coins.ph'
+COINS_API_KEY = "09Wvsxce4IVBYfW1yIn4OnvlstObgTXNEeV0i7jY"
+COINS_SECRET = "8PLwxbZpB3nW2N7kqRaIfkgja5kLTMO6VYUaJMLQ3Aea2yfv4K"
 
 DATABASES = {
     'default': {

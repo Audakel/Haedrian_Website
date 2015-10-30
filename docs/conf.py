aaf61dc1eee61a7bc5bdf87e56eadb0b8963a526
Aus Har <aus@mailinator.com>
2015-10-30 14:59:13 -0600

    theme

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ sys.path.append(os.path.abspath('../../lib/python2.7/site-packages/'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Haedrian_Website.settings.development")
 django.setup()
 
-html_style = '/default.css'
+
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -128,7 +128,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
